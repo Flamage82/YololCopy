088fb865fed691f7eafebf49e14b2bc140f92e93
DigitalLibrarian <likwid38225@gmail.com>
2021-08-21 13:39:46 -0500

    Update Program.cs
    * added copy mode
    * added clear mode
    * added command line arguments for the 3 invocations
    * fixed bug where it would not paste empty lines (some folks use empty lines for yolol script timing)
    * added alert sound at end of execution so user knows they are free to alt-tab

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -6,6 +6,7 @@ using System.Threading;
 using WindowsInput;
 using WindowsInput.Native;
 using TextCopy;
+using System.Text;
 
 namespace YololCopy.ConsoleApp
 {
@@ -13,11 +14,16 @@ namespace YololCopy.ConsoleApp
     {
         [DllImport("User32.dll")]
         private static extern bool SetForegroundWindow(IntPtr point);
+        private static readonly int Rows = 20;
+        private enum Invoke { Bad, Copy, Paste, Clear }
 
         public static void Main(string[] args)
         {
+            var invoke = CheckArgs(args);
+            if (invoke == Invoke.Bad) return;
+
             var text = ClipboardService.GetText();
-            if (string.IsNullOrWhiteSpace(text))
+            if (invoke == Invoke.Paste && string.IsNullOrWhiteSpace(text))
             {
                 Console.WriteLine("There is nothing on the clipboard to paste into Starbase");
                 return;
@@ -38,7 +44,80 @@ namespace YololCopy.ConsoleApp
 
             var inputSimulator = new InputSimulator();
 
-            foreach (var line in text.Split(new [] { "\r", "\n"}, StringSplitOptions.RemoveEmptyEntries))
+            switch (invoke)
+            {
+                case Invoke.Copy:
+                    DoCopy(inputSimulator);
+                    break;
+                case Invoke.Paste:
+                    DoPaste(inputSimulator, text);
+                    break;
+                case Invoke.Clear:
+                    DoClear(inputSimulator);
+                    break;
+            }
+
+            Console.WriteLine("Done\a");
+        }
+
+        private static Invoke CheckArgs(string [] args)
+        {
+            if (args.Length != 1 || !new string[] { "--copy", "--paste", "--clear" }.Contains(args[0]))
+            {
+                Console.WriteLine(
+                    string.Format(
+                        "USAGE: {0} {{--copy|--paste|==clear}}",
+                        System.AppDomain.CurrentDomain.FriendlyName));
+                return Invoke.Bad;
+            }
+            switch (args[0])
+            {
+                case "--copy":
+                    return Invoke.Copy;
+                case "--paste":
+                    return Invoke.Paste;
+                case "--clear":
+                    return Invoke.Clear;
+                default:
+                    throw new ArgumentException(string.Format("Unrecognized argument {0}", args[0]));
+            }
+        }
+
+        public static void DoClear(InputSimulator inputSimulator)
+        {
+            for(int i = 0; i < Rows; i++)
+            {
+                inputSimulator.Keyboard.KeyPress(VirtualKeyCode.DOWN);
+                Thread.Sleep(50);
+            }
+
+            for (int i = 0; i < Rows; i++)
+            {
+                inputSimulator.Keyboard.KeyPress(VirtualKeyCode.HOME);
+                Thread.Sleep(50);
+                inputSimulator.Keyboard.KeyDown(VirtualKeyCode.LSHIFT);
+                Thread.Sleep(50);
+                inputSimulator.Keyboard.KeyPress(VirtualKeyCode.END);
+                Thread.Sleep(50);
+                inputSimulator.Keyboard.KeyUp(VirtualKeyCode.LSHIFT);
+                Thread.Sleep(50);
+                inputSimulator.Keyboard.KeyPress(VirtualKeyCode.DELETE);
+                Thread.Sleep(50);
+                inputSimulator.Keyboard.KeyPress(VirtualKeyCode.UP);
+            }
+        }
+
+        public static void DoPaste(InputSimulator inputSimulator, string text)
+        {
+            for (int i = 0; i < Rows; i++)
+            {
+                inputSimulator.Keyboard.KeyPress(VirtualKeyCode.UP);
+                Thread.Sleep(50);
+            }
+            inputSimulator.Keyboard.KeyPress(VirtualKeyCode.HOME);
+            Thread.Sleep(50);
+            
+            foreach (var line in text.Split(new[] { Environment.NewLine }, StringSplitOptions.None))
             {
                 inputSimulator.Keyboard.ModifiedKeyStroke(VirtualKeyCode.LCONTROL, VirtualKeyCode.VK_X);
                 Thread.Sleep(50);
@@ -46,7 +125,44 @@ namespace YololCopy.ConsoleApp
                 inputSimulator.Keyboard.ModifiedKeyStroke(VirtualKeyCode.LCONTROL, VirtualKeyCode.VK_V);
                 Thread.Sleep(50);
                 inputSimulator.Keyboard.KeyPress(VirtualKeyCode.DOWN);
+                Thread.Sleep(50);
             }
+        }
+
+        public static void DoCopy(InputSimulator inputSimulator)
+        {
+            for (int i = 0; i < Rows; i++)
+            {
+                inputSimulator.Keyboard.KeyPress(VirtualKeyCode.UP);
+                Thread.Sleep(50);
+            }
+            var sb = new StringBuilder();
+            for(int i = 0; i < Rows; i++)
+            {
+                inputSimulator.Keyboard.KeyPress(VirtualKeyCode.HOME);
+                Thread.Sleep(50);
+
+
+                inputSimulator.Keyboard.KeyDown(VirtualKeyCode.LSHIFT);
+                Thread.Sleep(50);
+                inputSimulator.Keyboard.KeyPress(VirtualKeyCode.END);
+                Thread.Sleep(50);
+                inputSimulator.Keyboard.KeyUp(VirtualKeyCode.LSHIFT);
+                Thread.Sleep(50);
+
+                inputSimulator.Keyboard.KeyDown(VirtualKeyCode.LCONTROL);
+                Thread.Sleep(50);
+                inputSimulator.Keyboard.KeyPress(VirtualKeyCode.VK_C);
+                Thread.Sleep(50);
+                inputSimulator.Keyboard.KeyUp(VirtualKeyCode.LCONTROL);
+                Thread.Sleep(50);
+                sb.AppendLine(ClipboardService.GetText());
+
+                inputSimulator.Keyboard.KeyPress(VirtualKeyCode.DOWN);
+                Thread.Sleep(50);
+            }
+
+            ClipboardService.SetText(sb.ToString());
         }
     }
 }
